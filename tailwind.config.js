fb636fcd37a72e6cbb484e43f5f1920717e878ed
ChamathkaDissanayaka <chamathka.dissa@gmail.com>
2021-11-07 13:58:30 +0530

    tailwindcss installed

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    purge: ['./resources/**/*.blade.php'],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            colors: {
+                'dark-main': '#18191A',
+                'dark-second': '#242526',
+                'dark-third': '#3A3B3C',
+                'dark-first': '#B8BBBF',
+            },
+        },
+    },
+    variants: {
+        extend: {
+            display: ['group-hover'],
+            transform: ['group-hover'],
+            scale: ['group-hover'],
+        },
+    },
+    plugins: [
+        require('@tailwindcss/forms'),
+    ],
+}
