fc0c6d591242abca0fe645be3c59a3ee51cad465
ChamathkaDissanayaka <chamathka.dissa@gmail.com>
2021-11-08 15:42:21 +0530

    login.blade.php updated

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -25,8 +25,10 @@
                 </div>
 
                 <div class="w-full mt-16 md:mt-0 md:w-5/12">
-                    <div
-                        class="
+                    <form action="{{ route('login') }}" method="post">
+                        @csrf
+                        <div
+                            class="
 								relative
 								z-10
 								h-auto
@@ -39,12 +41,13 @@
 								px-7
 								rounded-3xl
 							"
-                    >
-                        <h3 class="mb-6 text-2xl font-medium">Sign in to your Account</h3>
-                        <input
-                            type="text"
-                            name="email"
-                            class="
+                        >
+                            <h3 class="mb-6 text-2xl font-medium">Sign in to your Account</h3>
+                            <div>
+                                <input
+                                    type="email"
+                                    name="email"
+                                    class="
 									block
 									w-full
 									px-4
@@ -52,13 +55,24 @@
 									mb-4
 									border border-gray-200
 									focus:outline-none focus:border-pink-400 focus:ring-0
+									@error('email')
+                                        border-red-500
+                                     @enderror
 								"
-                            placeholder="Email address"
-                        />
-                        <input
-                            type="password"
-                            name="password"
-                            class="
+                                    placeholder="Email address"
+                                />
+                                @error('email')
+                                <p class="text-red-500">{{ $message }}</p>
+                                @enderror
+                            </div>
+
+
+
+                           <div>
+                               <input
+                                   type="password"
+                                   name="password"
+                                   class="
 									block
 									w-full
 									px-4
@@ -66,15 +80,23 @@
 									mb-2
 									border border-gray-200
 									focus:outline-none focus:border-pink-400 focus:ring-0
+									@error('password')
+                                       border-red-500
+@enderror
 								"
-                            placeholder="Password"
-                        />
+                                   placeholder="Password"
+                               />
+                               @error('password')
+                               <p class="text-red-500">{{ $message }}</p>
+                               @enderror
+                           </div>
 
-                        <div class="py-2 px-2 flex justify-between">
-                            <label class="inline-flex items-center">
-                                <input
-                                    type="checkbox"
-                                    class="
+
+                            <div class="py-2 px-2 flex justify-between">
+                                <label class="inline-flex items-center">
+                                    <input
+                                        type="checkbox"
+                                        class="
 											rounded
 											border-gray-300
 											text-pink-600
@@ -85,53 +107,56 @@
 											focus:ring-pink-200
 											focus:ring-opacity-50
 										"
-                                />
-                                <span class="ml-2 text-gray-800 text-sm cursor-pointer"
-                                >Keep me signed in</span
-                                >
-                            </label>
+                                    />
+                                    <span class="ml-2 text-gray-800 text-sm cursor-pointer"
+                                    >Keep me signed in</span
+                                    >
+                                </label>
 
-                            <a
-                                href="#"
-                                class="
+                                @if (Route::has('password.request'))
+                                <a
+                                    href="{{ route('password.request') }}"
+                                    class="
 										text-sm text-gray-8100
 										hover:text-pink-500 hover:underline
 									"
-                            >
-                                Forgot Password?
-                            </a>
-                        </div>
-
-                        <div class="block mt-2">
-                            <button
-                                class="w-full px-3 py-4 font-medium text-white bg-pink-500"
-                            >
-                                Log Me In
-                            </button>
-                        </div>
-                        <p class="w-full mt-4 text-sm text-center text-gray-500">
-                            Don't have an account?
-                            <button class="text-pink-500 hover:text-pink-600 underline">
-                                Sign up here
-                            </button>
-                        </p>
-
-                        <div class="grid grid-cols-12 gap-4 items-center py-4">
-                            <div class="col-span-5">
-                                <hr class="border" />
+                                >
+                                    Forgot Password?
+                                </a>
+                                @endif
                             </div>
-                            <div class="col-span-2 text-center">
-                                <span class="text-gray-400 font-semibold">Or</span>
-                            </div>
-                            <div class="col-span-5">
-                                <hr class="border" />
-                            </div>
-                        </div>
 
-                        <div class="space-y-3">
-                            <a
-                                href="#"
-                                class="
+                            <div class="block mt-2">
+                                <button
+                                    type="submit"
+                                    class="w-full px-3 py-4 font-medium text-white bg-pink-500"
+                                >
+                                    Log Me In
+                                </button>
+                            </div>
+                            <p class="w-full mt-4 text-sm text-center text-gray-500">
+                                Don't have an account?
+                                <button class="text-pink-500 hover:text-pink-600 underline" href="{{ route('register') }}">
+                                    Sign up here
+                                </button>
+                            </p>
+
+                            <div class="grid grid-cols-12 gap-4 items-center py-4">
+                                <div class="col-span-5">
+                                    <hr class="border" />
+                                </div>
+                                <div class="col-span-2 text-center">
+                                    <span class="text-gray-400 font-semibold">Or</span>
+                                </div>
+                                <div class="col-span-5">
+                                    <hr class="border" />
+                                </div>
+                            </div>
+
+                            <div class="space-y-3">
+                                <a
+                                    href="#"
+                                    class="
 										justify-center
 										flex
 										px-4
@@ -148,28 +173,28 @@
 										hover:bg-gray-50
 										focus:outline-none focus:shadow-none
 									"
-                            >
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    class="h-6 w-6 mr-2"
-                                    viewBox="0 0 48 48"
-                                    style="fill: #000000"
                                 >
-                                    <path
-                                        fill="#3f51b5"
-                                        d="M24 4A20 20 0 1 0 24 44A20 20 0 1 0 24 4Z"
-                                    ></path>
-                                    <path
-                                        fill="#fff"
-                                        d="M29.368,24H26v12h-5V24h-3v-4h3v-2.41c0.002-3.508,1.459-5.59,5.592-5.59H30v4h-2.287 C26.104,16,26,16.6,26,17.723V20h4L29.368,24z"
-                                    ></path>
-                                </svg>
-                                <span>Login with Facebook</span>
-                            </a>
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        class="h-6 w-6 mr-2"
+                                        viewBox="0 0 48 48"
+                                        style="fill: #000000"
+                                    >
+                                        <path
+                                            fill="#3f51b5"
+                                            d="M24 4A20 20 0 1 0 24 44A20 20 0 1 0 24 4Z"
+                                        ></path>
+                                        <path
+                                            fill="#fff"
+                                            d="M29.368,24H26v12h-5V24h-3v-4h3v-2.41c0.002-3.508,1.459-5.59,5.592-5.59H30v4h-2.287 C26.104,16,26,16.6,26,17.723V20h4L29.368,24z"
+                                        ></path>
+                                    </svg>
+                                    <span>Login with Facebook</span>
+                                </a>
 
-                            <a
-                                href="#"
-                                class="
+                                <a
+                                    href="#"
+                                    class="
 										justify-center
 										flex
 										px-4
@@ -186,34 +211,35 @@
 										hover:bg-gray-50
 										focus:outline-none focus:shadow-none
 									"
-                            >
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    viewBox="0 0 48 48"
-                                    style="fill: #000000"
-                                    class="h-6 w-6 mr-2"
                                 >
-                                    <path
-                                        fill="#fbc02d"
-                                        d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12	s5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24s8.955,20,20,20	s20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"
-                                    ></path>
-                                    <path
-                                        fill="#e53935"
-                                        d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039	l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"
-                                    ></path>
-                                    <path
-                                        fill="#4caf50"
-                                        d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36	c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"
-                                    ></path>
-                                    <path
-                                        fill="#1565c0"
-                                        d="M43.611,20.083L43.595,20L42,20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571	c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"
-                                    ></path>
-                                </svg>
-                                <span>Login with Google</span>
-                            </a>
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        viewBox="0 0 48 48"
+                                        style="fill: #000000"
+                                        class="h-6 w-6 mr-2"
+                                    >
+                                        <path
+                                            fill="#fbc02d"
+                                            d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12	s5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24s8.955,20,20,20	s20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"
+                                        ></path>
+                                        <path
+                                            fill="#e53935"
+                                            d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039	l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"
+                                        ></path>
+                                        <path
+                                            fill="#4caf50"
+                                            d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36	c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"
+                                        ></path>
+                                        <path
+                                            fill="#1565c0"
+                                            d="M43.611,20.083L43.595,20L42,20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571	c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"
+                                        ></path>
+                                    </svg>
+                                    <span>Login with Google</span>
+                                </a>
+                            </div>
                         </div>
-                    </div>
+                    </form>
                 </div>
             </div>
         </div>
